9535814202355dcc8bfa020a8b7bc1c2b818d402
Angelo Cordon <angelocordon@gmail.com>
2015-07-27 16:44:05 -0700

    Added jQuery Toggle

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,5 +1,13 @@
 $( document ).ready(function() {
 
-  //
+  $(".toggle").click(function() {
+    $(".intro").removeClass("intro-open");
+    $(".intro").addClass("intro-close");
+  });
+
+  $(".close").click(function() {
+    $(".intro").removeClass("intro-close");
+    $(".intro").addClass("intro-open");
+  });
 
 });
