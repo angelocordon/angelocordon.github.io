9c3302aafaa12eac90c4d215b8e21d96f4090e9d
Angelo Cordon <angelocordon@gmail.com>
2017-12-30 11:19:21 -0800

    Initial set up of Nuxt

---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <section class="section">
+    <h1 class="title">
+      Hi, I'm Angelo
+    </h1>
+    <h2 class="subtitle">
+      I sit somewhere between digital product design and front end development.
+    </h2>
+    <p class="content">
+      Sometimes, I sit between my kids when they can’t agree on anything. Though the best times are when I can sit next to my wife and eat dinner in peace.
+    </p>
+    <p class="content">
+      Currently, I’m designing and developing at USEED. Previously, I co-founded AVV, Filipinos Forward and a few other projects.
+    </p>
+    <p class="content">
+      Want to chat? Happy to get in touch.
+    </p>
+    <p class="content">
+      Elsewhere: Dribbble | GitHub | Medium
+    </p>
+  </section>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
