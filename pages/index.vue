01f1cd2753362597c8431f4f4f667a92d64181f2
Angelo Cordon <angelocordon@gmail.com>
2017-12-30 12:59:55 -0800

    Add placeholder links to main copy
    Move global stylesheets to default.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,26 @@
 <template>
   <div class="container">
-    <h1 class="title is-spaced">
-      Hi, I'm Angelo
+    <h1 class="title is-spaced is-1">
+      Hi, I'm <a>Angelo</a>
     </h1>
-    <h2 class="subtitle">
+    <h2 class="subtitle is-3">
       I sit somewhere between digital product design and front end development.
     </h2>
     <p class="content">
       Sometimes, I sit between my kids when they can’t agree on anything. Though the best times are when I can sit next to my wife and eat dinner in peace.
     </p>
     <p class="content">
-      Currently, I’m designing and developing at USEED. Previously, I co-founded AVV, Filipinos Forward and a few other projects.
+      Currently, I’m designing and developing at <a>USEED</a>. Previously, I co-founded <a>AVV</a>, <a>Filipinos Forward</a> and a few <a>other projects</a>.
     </p>
     <p class="content">
-      Want to chat? Happy to get in touch.
+      Want to chat? Happy to <a>get in touch</a>.
     </p>
     <p class="content">
-      Elsewhere: Dribbble | GitHub | Medium
+      Elsewhere: <a href="https://www.dribbble.com/angelocordon">Dribbble</a> | <a href="https://www.github.com/angelocordon">GitHub</a> | <a href="https://www.medium.com/@angelocordon">Medium</a>
     </p>
   </div>
 </template>
 
-<script>
-  export default {
-
-  }
-</script>
+<style scoped>
+  .subtitle.is-3 { line-height: 3rem; }
+</style>
