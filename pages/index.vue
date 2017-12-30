03d008c3b532e828a0c6a38411ef66a34398d6ba
Angelo Cordon <angelocordon@gmail.com>
2017-12-30 14:28:35 -0800

    Add columns

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,27 @@
 <template>
   <div class="container">
-    <h1 class="title is-spaced is-1">
-      Hi, I'm <a>Angelo</a>
-    </h1>
-    <h2 class="subtitle is-3">
-      I sit somewhere between digital product design and front end development.
-    </h2>
-    <p class="content">
-      Sometimes, I sit between my kids when they can’t agree on anything. Though the best times are when I can sit next to my wife and eat dinner in peace.
-    </p>
-    <p class="content">
-      Currently, I’m designing and developing at <a>USEED</a>. Previously, I co-founded <a>AVV</a>, <a>Filipinos Forward</a> and a few <a>other projects</a>.
-    </p>
-    <p class="content">
-      Want to get in touch? <a>Let's connect</a>.
-    </p>
-    <p class="content">
-      Elsewhere: <a href="https://www.dribbble.com/angelocordon">Dribbble</a> | <a href="https://www.github.com/angelocordon">GitHub</a> | <a href="https://www.medium.com/@angelocordon">Medium</a>
-    </p>
+    <div class="columns">
+      <div class="column is-three-fifths-desktop is-half-widescreen">
+        <h1 class="title is-spaced is-1">
+          Hi, I'm <a>Angelo</a>
+        </h1>
+        <h2 class="subtitle is-3">
+          I sit somewhere between digital product design and front end development.
+        </h2>
+        <p class="content">
+          Sometimes, I sit between my kids when they can’t agree on anything. Though the best times are when I can sit next to my wife and eat dinner in peace.
+        </p>
+        <p class="content">
+          Currently, I’m designing and developing at <a>USEED</a>. Previously, I co-founded <a>AVV</a>, <a>Filipinos Forward</a> and a few <a>other projects</a>.
+        </p>
+        <p class="content">
+          Want to get in touch? <a>Let's connect</a>.
+        </p>
+        <p class="content">
+          Elsewhere: <a href="https://www.dribbble.com/angelocordon">Dribbble</a> | <a href="https://www.github.com/angelocordon">GitHub</a> | <a href="https://www.medium.com/@angelocordon">Medium</a>
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
